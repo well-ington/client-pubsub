b7c8d0b9305fccccc087e5d30a9f736d04dbe1db
well-ington <celso.w.dias.a@gmail.com>
2020-10-08 14:23:52 -0300

    test: adding min-length to the validation constructor

--- src/validation/validators/validation-constructor/validation-constructor.spec.ts
+++ src/validation/validators/validation-constructor/validation-constructor.spec.ts
@@ -1,4 +1,5 @@
 import { RequiredFieldValidation, ValidationConstructor as sut, EmailValidation } from ".."
+import { MinLengthValidator } from "../min-length/min-length"
 
 describe('ValidationConstructor', () => {
 
@@ -12,8 +13,8 @@ describe('ValidationConstructor', () => {
     expect(validations).toEqual([new EmailValidation('any_field')])
   })
 
-  // test('Should return EmailValidation', () => {
-  //   const validations = sut.field('any_field').minLength().build()
-  //   expect(validations).toEqual([new EmailValidation('any_field')])
-  // })
+  test('Should return EmailValidation', () => {
+    const validations = sut.field('any_field').minLength(5).build()
+    expect(validations).toEqual([new MinLengthValidator('any_field', 5)])
+  })
 })
