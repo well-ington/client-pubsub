1cdae81d6e6c346419aeda1420a642d0ddf9402e
well-ington <celso.w.dias.a@gmail.com>
2020-10-08 14:00:18 -0300

    feat: adding composite field validator

---
+++ src/validation/validators/validator-composite/validator-composite.ts
@@ -0,0 +1,17 @@
+import { Ivalidation } from '@/presentation/protocols'
+import { IfieldValidation } from '@/validation/protocols/field-validation'
+
+export class ValidatorComposite implements Ivalidation {
+  constructor (private readonly validators: IfieldValidation[]) {}
+
+  validate (fieldName: string, fieldValue: string): string {
+    const validators = this.validators.filter(v => v.field === fieldName)
+    for (const valid of validators) {
+      const error = valid.validate(fieldValue)
+      if (error) {
+        return error.message
+      }
+    }
+    return null
+  }
+}
