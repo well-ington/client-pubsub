fc6558936104647a7359fa13319a3219879e5fed
well-ington <celso.w.dias.a@gmail.com>
2020-10-08 14:20:08 -0300

    test: adding email to the validation constructor

--- src/validation/validators/validation-constructor/validation-constructor.spec.ts
+++ src/validation/validators/validation-constructor/validation-constructor.spec.ts
@@ -1,8 +1,19 @@
-import { RequiredFieldValidation, ValidationConstructor as sut } from ".."
+import { RequiredFieldValidation, ValidationConstructor as sut, EmailValidation } from ".."
 
 describe('ValidationConstructor', () => {
+
   test('Should return Required Field Validation', () => {
     const validations = sut.field('any_field').required().build()
     expect(validations).toEqual([new RequiredFieldValidation('any_field')])
   })
+
+  test('Should return EmailValidation', () => {
+    const validations = sut.field('any_field').email().build()
+    expect(validations).toEqual([new EmailValidation('any_field')])
+  })
+
+  // test('Should return EmailValidation', () => {
+  //   const validations = sut.field('any_field').minLength().build()
+  //   expect(validations).toEqual([new EmailValidation('any_field')])
+  // })
 })
