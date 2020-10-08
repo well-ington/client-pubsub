bc81bf1dad85bc995fda4402fdaf9e4ec6c163b7
well-ington <celso.w.dias.a@gmail.com>
2020-10-08 13:58:41 -0300

    feat: adding email field validator

---
+++ src/validation/validators/email/email-validation.ts
@@ -0,0 +1,11 @@
+import {IfieldValidation} from "@/validation/protocols/field-validation"
+import {InvalidFieldError} from '@/validation/errors/invalid-field-error'
+
+export class EmailValidation implements IfieldValidation {
+  constructor (readonly field: string) {}
+
+  validate (input: object): Error {
+    const regexValidator = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/
+    return (!input[this.field] || regexValidator.test(input[this.field]) ? null : new InvalidFieldError())
+  }
+ }
