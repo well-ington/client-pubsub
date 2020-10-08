a7fc0e9006a84b366519b6122d7f836bc6212363
well-ington <celso.w.dias.a@gmail.com>
2020-10-08 13:51:58 -0300

    feat: added router component

---
+++ src/presentation/components/router/router.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Login } from '@/presentation/pages'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+
+const Router: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/login" exact component={Login} />
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default Router
