7308573479d9a361e3b5d846c931a27cc8dad9bf
well-ington <celso.w.dias.a@gmail.com>
2020-10-13 01:49:59 -0300

    feat: added index to Home components

---
+++ src/presentation/pages/home/components/index.ts
@@ -0,0 +1,4 @@
+export { default as MessageCard } from './message-card/message-card'
+export { default as PublishMessage } from './publish-message/publish-message'
+export { default as Header } from './header/header'
+export { default as Footer } from './footer/footer'
